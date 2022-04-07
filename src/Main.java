6bb0189cea8b131356cd87bf0fd2d1c576615e1a
Ivan <ivan4ik.ru70@gmail.com>
2022-04-07 15:41:04 +0700

    реализовал калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws RuntimeException {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1 ,2);
+        int b = calc.minus.apply(1,1);
+        int c = 0;
+
+//        Реализация исключения при делении на ноль
+        try {
+            c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на 0 не возможно");
+        }
+
+        int x = calc.abs.apply(-10);
+        int y = calc.pow.apply(c);
+
+        calc.println.accept(x);
+        calc.println.accept(y);
     }
 }
