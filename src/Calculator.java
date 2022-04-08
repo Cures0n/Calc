7b3228d26425ef4aaad0be687d6800a964c28258
Ivan <ivan4ik.ru70@gmail.com>
2022-04-08 09:24:55 +0700

    добавил второй способ решения

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,10 +6,14 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+//    BinaryOperator<Integer> devide = (x, y) -> x / y;
 
 //    Ошибка в данном случаем возникает из-за не возможности деления на ноль
-//    Решение реализовано путем выброса исключения с описанной ошибкой
+//    1-e Решение реализовано путем выброса исключения с описанной ошибкой
+//    и реализовано в классе main
+//    2-e решение можно сделать через тернарный оператор с выбросом ноля,
+//    но математически это не совсем корректно
+    BinaryOperator<Integer> devide = (x, y) -> y > 0 ? x / y : 0;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main {
 
         int a = calc.plus.apply(1 ,2);
         int b = calc.minus.apply(1,1);
-        int c = 0;
+        int c = calc.devide.apply(a, b);
 
 //        Реализация исключения при делении на ноль
-        try {
-            c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на 0 не возможно");
-        }
-
+//        try {
+//            c = calc.devide.apply(a, b);
+//            calc.println.accept(c);
+//        } catch (ArithmeticException e) {
+//            System.out.println("Деление на 0 не возможно");
+//        }
+        calc.println.accept(c);
         int x = calc.abs.apply(-10);
         int y = calc.pow.apply(c);
 
